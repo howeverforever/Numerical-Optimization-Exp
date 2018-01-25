ef2c38c83a2a254538a0aff937f4ff5d90614311
howeverforever <longbiau@hotmail.com.tw>
2018-01-25 15:24:10 +0800

    Complete the case: fixed points.

---
+++ sec10.1.py
@@ -0,0 +1,36 @@
+import numpy as np
+from numpy import linalg
+
+import pandas as pd
+import math
+np.set_printoptions(suppress=True)
+
+TOR = pow(10.0, -5)
+
+
+def fix_point(x):
+    """
+    given x_0 in R^3 as a starting point.
+
+    :param x: x_0 as described
+    :return: the minimizer x* of f
+    """
+    y = np.zeros(len(x))
+    while True:
+        y[0] = math.cos(x[1] * x[2]) / 3.0 + 1.0 / 6.0
+        y[1] = math.sqrt(x[0] * x[0] + math.sin(x[2]) + 1.06) / 9.0 - 0.1
+        y[2] = -math.exp(-x[0] * x[1]) / 20.0 - (10.0 * math.pi - 3.0) / 60.0
+        if linalg.norm(x - y, np.inf) < TOR:
+            break
+        x = y
+
+    print(y)
+
+
+def main():
+    fix_point(np.array([0.1, 0.1, -0.1]))
+    return
+
+
+if __name__ == '__main__':
+    main()
