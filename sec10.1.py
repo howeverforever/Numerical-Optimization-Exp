06d1fb6cbfc014e2ed6d3b413a90e9d7c7a492da
howeverforever <longbiau@hotmail.com.tw>
2018-01-26 13:00:00 +0800

    Show the tables with experiments.

--- sec10.1.py
+++ sec10.1.py
@@ -1,49 +1,106 @@
 import numpy as np
 from numpy import linalg
-
+from abc import abstractmethod
 import pandas as pd
 import math
-np.set_printoptions(suppress=True)
+
+pd.options.display.float_format = '{:,.8f}'.format
+np.set_printoptions(suppress=True, precision=8)
 
 TOR = pow(10.0, -5)
 
 
-def fix_point(x):
-    """
-    given x_0 in R^3 as a starting point.
+class FixedPointMethod(object):
 
-    :param x: x_0 as described
-    :return: the minimizer x* of f
-    """
-    y = np.zeros(len(x))
-    while True:
-        y[0] = math.cos(x[1] * x[2]) / 3.0 + 1.0 / 6.0
-        y[1] = math.sqrt(x[0] * x[0] + math.sin(x[2]) + 1.06) / 9.0 - 0.1
-        y[2] = -math.exp(-x[0] * x[1]) / 20.0 - (10.0 * math.pi - 3.0) / 60.0
-        if linalg.norm(x - y, np.inf) < TOR:
-            break
-        x = y
+    def __init__(self):
+        return
 
-    print(y)
+    @abstractmethod
+    def f(self, x):
+        return NotImplementedError('Implement f()!')
+
+    @abstractmethod
+    def run(self, x):
+        return NotImplementedError('Implement run()!')
 
 
-def fix_point_acc(x):
-    y = np.zeros(len(x))
-    while True:
-        y[0] = math.cos(x[1] * x[2]) / 3.0 + 1.0 / 6.0
-        y[1] = math.sqrt(y[0] * y[0] + math.sin(x[2]) + 1.06) / 9.0 - 0.1
-        y[2] = -math.exp(-y[0] * y[1]) / 20.0 - (10.0 * math.pi - 3.0) / 60.0
-        if linalg.norm(x - y, np.inf) < TOR:
-            break
-        x = y
+class FixedPoint(FixedPointMethod):
 
-    print(y)
+    def __init__(self):
+        super(FixedPointMethod, self).__init__()
+
+    def f(self, x):
+        sol = np.zeros(len(x))
+        sol[0] = math.cos(x[1] * x[2]) / 3.0 + 1.0 / 6.0
+        sol[1] = math.sqrt(x[0] * x[0] + math.sin(x[2]) + 1.06) / 9.0 - 0.1
+        sol[2] = -math.exp(-x[0] * x[1]) / 20.0 - (10.0 * math.pi - 3.0) / 60.0
+        return sol
+
+    def run(self, x):
+        """
+        given x_0 in R^3 as a starting point.
+
+        :param x: x_0 as described
+        :return: the minimizer x* of f
+        """
+        df = pd.DataFrame(columns=['x' + str(i + 1) for i in range(len(x))] + ['residual'])
+
+        row = len(df)
+        df.loc[row] = [xe for xe in x] + [np.nan]
+
+        while True:
+            y = self.f(x)
+            residual = linalg.norm(x - y, np.inf)
+
+            row = len(df)
+            df.loc[row] = [ye for ye in y] + [residual]
+            if residual < TOR:
+                break
+            x = y
+
+        print(df)
+
+
+class FixedPointAcceleration(FixedPointMethod):
+
+    def __init__(self):
+        super(FixedPointMethod, self).__init__()
+
+    def f(self, x):
+        sol = np.zeros(len(x))
+        sol[0] = math.cos(x[1] * x[2]) / 3.0 + 1.0 / 6.0
+        sol[1] = math.sqrt(sol[0] * sol[0] + math.sin(x[2]) + 1.06) / 9.0 - 0.1
+        sol[2] = -math.exp(-sol[0] * sol[1]) / 20.0 - (10.0 * math.pi - 3.0) / 60.0
+        return sol
+
+    def run(self, x):
+        """
+        given x_0 in R^3 as a starting point.
+
+        :param x: x_0 as described
+        :return: the minimizer x* of f
+        """
+        df = pd.DataFrame(columns=['x' + str(i + 1) for i in range(len(x))] + ['residual'])
+
+        row = len(df)
+        df.loc[row] = [xe for xe in x] + [np.nan]
+        while True:
+            y = self.f(x)
+            residual = linalg.norm(x - y, np.inf)
+
+            row = len(df)
+            df.loc[row] = [ye for ye in y] + [residual]
+            if residual < TOR:
+                break
+            x = y
+
+        print(df)
 
 
 def main():
     x0 = np.array([0.1, 0.1, -0.1])
-    fix_point(x0)
-    fix_point_acc(x0)
+    FixedPoint().run(x0)
+    FixedPointAcceleration().run(x0)
     return
 
 
--- sec10.2.py
+++ sec10.2.py
@@ -1,58 +1,88 @@
 import numpy as np
 from numpy import linalg
-
+from abc import abstractmethod
 import pandas as pd
 import math
 
-np.set_printoptions(suppress=True, precision=12)
+pd.options.display.float_format = '{:,.10f}'.format
+np.set_printoptions(suppress=True, precision=10)
 
 TOR = pow(10.0, -5)
 
 
-def f(x):
-    sol = np.zeros(len(x))
-    sol[0] = 3 * x[0] - math.cos(x[1] * x[2]) - 1.0 / 2.0
-    sol[1] = pow(x[0], 2) - 81 * pow(x[1] + 0.1, 2) + math.sin(x[2]) + 1.06
-    sol[2] = math.exp(-x[0] * x[1]) + 20 * x[2] + (10 * math.pi - 3.0) / 3.0
-    return sol
+class NewtonMethod(object):
+
+    def __init__(self):
+        return
+
+    @abstractmethod
+    def f(self, x):
+        return NotImplementedError('Implement f()!')
+
+    @abstractmethod
+    def jacobian(self, x):
+        return NotImplementedError('Implement jacobian()!')
+
+    @abstractmethod
+    def run(self, x):
+        return NotImplementedError('Implement run()!')
 
 
-def jacobian(x):
-    jac = np.zeros(shape=(3, 3))
-    jac[0][0] = 3.0
-    jac[0][1] = x[2] * math.sin(x[1] * x[2])
-    jac[0][2] = x[1] * math.sin(x[1] * x[2])
-    jac[1][0] = 2 * x[0]
-    jac[1][1] = -162 * (x[1] + 0.1)
-    jac[1][2] = math.cos(x[2])
-    jac[2][0] = -x[1] * math.exp(-x[0] * x[1])
-    jac[2][1] = -x[0] * math.exp(-x[0] * x[1])
-    jac[2][2] = 20
-    return jac
+class Newton(NewtonMethod):
 
+    def __init__(self):
+        super(NewtonMethod, self).__init__()
 
-def newton_method(x):
-    """
-    given x_0 in R^3 as a starting point.
+    def f(self, x):
+        sol = np.zeros(len(x))
+        sol[0] = 3 * x[0] - math.cos(x[1] * x[2]) - 1.0 / 2.0
+        sol[1] = pow(x[0], 2) - 81 * pow(x[1] + 0.1, 2) + math.sin(x[2]) + 1.06
+        sol[2] = math.exp(-x[0] * x[1]) + 20 * x[2] + (10 * math.pi - 3.0) / 3.0
+        return sol
 
-    :param x: x_0 as described
-    :return: the minimizer x* of f
-    """
-    while True:
-        print(x)
-        jac = jacobian(x)
-        ff = -f(x)
-        y = linalg.solve(jac, ff)
-        nx = x + y
-        if linalg.norm(x - nx, np.inf) < TOR:
-            break
-        x = nx
-    return x
+    def jacobian(self, x):
+        jac = np.zeros(shape=(3, 3))
+        jac[0][0] = 3.0
+        jac[0][1] = x[2] * math.sin(x[1] * x[2])
+        jac[0][2] = x[1] * math.sin(x[1] * x[2])
+        jac[1][0] = 2 * x[0]
+        jac[1][1] = -162 * (x[1] + 0.1)
+        jac[1][2] = math.cos(x[2])
+        jac[2][0] = -x[1] * math.exp(-x[0] * x[1])
+        jac[2][1] = -x[0] * math.exp(-x[0] * x[1])
+        jac[2][2] = 20
+        return jac
+
+    def run(self, x):
+        """
+        given x_0 in R^3 as a starting point.
+
+        :param x: x_0 as described
+        :return: the minimizer x* of f
+        """
+        df = pd.DataFrame(columns=['x' + str(i + 1) for i in range(len(x))] + ['residual'])
+
+        row = len(df)
+        df.loc[row] = [xe for xe in x] + [np.nan]
+
+        while True:
+            jac = self.jacobian(x)
+            f = -self.f(x)
+            y = linalg.solve(jac, f)
+            nx = x + y
+            residual = linalg.norm(x - nx, np.inf)
+            row = len(df)
+            df.loc[row] = [nxe for nxe in nx] + [residual]
+            if residual < TOR:
+                break
+            x = nx
+
+        print(df)
 
 
 def main():
     x0 = np.array([0.1, 0.1, -0.1])
-    newton_method(x0)
+    Newton().run(x0)
     return
 
 
