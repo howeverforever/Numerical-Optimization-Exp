74ef2e6608b91a10d28674a5b089b713aa2519a0
howeverforever <longbiau@hotmail.com.tw>
2018-02-03 12:48:22 +0800

    Write 1D fixed-point.

--- sec10.1.py
+++ sec10.1.py
@@ -8,6 +8,7 @@ pd.options.display.float_format = '{:,.8f}'.format
 np.set_printoptions(suppress=True, precision=8)
 
 TOR = pow(10.0, -5)
+MAX_ITR = 30
 
 
 class FixedPointMethod(object):
@@ -105,10 +106,86 @@ class FixedPointAcceleration(FixedPointMethod):
         print(df)
 
 
+class FixedPoint1D(FixedPointMethod):
+
+    def __init__(self):
+        super(FixedPointMethod, self).__init__()
+
+    def f(self, x):
+        return math.cos(x)
+
+    def g1(self, x):
+        return np.array(x - pow(x, 3) - 4 * pow(x, 2) + 10)
+
+    def g2(self, x):
+        return np.array(math.sqrt(10 / x - 4 * x))
+
+    def g3(self, x):
+        return np.array(math.sqrt(10 - pow(x, 3)) / 2)
+
+    def g4(self, x):
+        return np.array(math.sqrt(10 / (4 + x)))
+
+    def g5(self, x):
+        return np.array(x - (pow(x, 3) + 4 * pow(x, 2) - 10) / (3 * pow(x, 2) + 8 * x))
+
+    def run2(self, x0):
+        df = pd.DataFrame(columns=['(sp) f(x)'])
+        row = len(df)
+        x = x0
+        df.loc[row] = [x]
+        for k in range(MAX_ITR):
+            try:
+                y = self.f(x)
+            except ValueError:
+                break
+            residual = math.fabs(x - y)
+            x = y
+
+            row = len(df)
+            df.loc[row] = [y]
+            if residual < TOR or x > 1e9:
+                break
+        return df
+
+    def run(self, x0):
+        """
+        given x_0 in R^3 as a starting point.
+
+        :param x: x_0 as described
+        :return: the minimizer x* of f
+        """
+        g = [self.g1, self.g2, self.g3, self.g4, self.g5]
+
+        total_df = None
+        for j in range(len(g)):
+            df = pd.DataFrame(columns=['g' + str(j + 1) + '(x)'])
+            row = len(df)
+            x = x0
+            df.loc[row] = [x]
+            for k in range(MAX_ITR):
+                try:
+                    y = np.array(g[j](x))
+                except ValueError:
+                    break
+                residual = math.fabs(x - y)
+                x = y
+
+                row = len(df)
+                df.loc[row] = [y]
+                if residual < TOR or x > 1e9:
+                    break
+            total_df = df if total_df is None else pd.concat([total_df, df], axis=1)
+
+
 def main():
     x0 = np.array([0.1, 0.1, -0.1])
-    FixedPoint().run(x0)
-    FixedPointAcceleration().run(x0)
+    x10 = 1.5
+    # FixedPoint().run(x0)
+    # FixedPointAcceleration().run(x0)
+    # FixedPoint1D().run(x10)
+    FixedPoint1D().run2(math.pi / 4)
+
     return
 
 
